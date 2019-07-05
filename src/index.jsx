867b653718a48bc61d76830ab593a7eaf1ad9d42
Kyle Harrington <harrington.kyle@gmail.com>
2019-07-05 15:11:55 -0700

    add more packages and Header

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './App';
 import { AppContainer } from 'react-hot-loader';
 
 const render = (Component) => {
