d7fe9693c7523d2e3280e89d7a1808a4483f4826
Kyle Harrington <harrington.kyle@gmail.com>
2019-07-07 18:00:50 -0700

    apply some styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,10 +3,16 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <div></div>
         <div>
+            <style jsx>{`
+                div {
+                    text-align: center;
+                    text-decoration: underline;
+                }
+            `}</style>
             <h1>BrewHaven</h1>
             <Link to='/'>Home</Link> | <Link to='/newkeg'>Add New Keg</Link> | <Link to='/editkeg'>Edit Keg(s)</Link>
+            <hr/>
         </div>
     );
 }
--- src/Keg.jsx
+++ src/Keg.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import barrel from './assets/images/barrel.jpg';
 
 function Keg(props) {
     return (
-        <div style={{ display: 'inline-block'}}>
+        <div style={{ display: 'inline-block', backgroundImage: 'url(' + barrel + ')', backgroundSize: 'contain', backgroundRepeat: 'no-repeat'}}>
             <style global jsx>{`
                 .the-keg{
-                    margin: 20px;
+                    margin: 10px;
                     width: 120px;
                     color: blue;
+                    text-align: center;
+                    border: 2px solid brown;
+                    border-radius: 5px;
                 }
-                `}</style>
-                <div className='the-keg'>
-                    <h3>{props.beerName}</h3>
-                    <h4>{props.breweryName}</h4>
-                    <h5>{props.cost}</h5>
-                    <h5>{props.abv}</h5>
-                </div>
+            `}</style>
+            <div className='the-keg'>
+                <h3>{props.beerName}</h3>
+                <h4>{props.breweryName}</h4>
+                <h5>{props.cost}</h5>
+                <h5>{props.abv} ABV</h5>
+                <button>Sell Pint</button>
+            </div>
         </div>
     );
 }
